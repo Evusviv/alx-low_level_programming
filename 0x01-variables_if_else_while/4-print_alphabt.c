9aa7325eb9308374eff0636fe53fc74f4bcb3e9a
Evusviv <evusviv@gmail.com>
2022-03-12 07:29:29 -0800

    Print Alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - print alphabet except q and e
- * main - print alphabet except q and e
- * Return: 0
- * Return: 0
- */
+*main - print alphabet except q and e
+*main - print alphabet except q and e
+* Return: 0
+* Return: 0
+*/
 int main(void)
 {
 int x = 'a';
