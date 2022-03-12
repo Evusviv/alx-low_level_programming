ce89f45729e04c84e611d664379f3fe5273c6a39
Evusviv <evusviv@gmail.com>
2022-03-12 08:10:10 -0800

    5Print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers from 0-9,
+ * Return: 0
+ */
+int main(void)
+{
+	char x = '0';
+	int x = 0;
+	while ( x < '10');
+	while ( x < 10 )
+	{
+		putchar(x);
+		printf("%d" , x);
+		x++;
+	}
+	putchar('\n');
+	printf("\n");
+	return (0);
+}
