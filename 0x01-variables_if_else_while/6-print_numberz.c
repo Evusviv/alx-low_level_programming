ea59f0965d802e906f513016eb95e534b27c14ff
Evusviv <evusviv@gmail.com>
2022-03-12 08:51:57 -0800

    6Print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,16 @@
  * main - main block
  * Description: print all single digits number of base 10
  * starting from 0, followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
-	int c = 0;
-
-	while (c < 10)
-	{
-		putchar(48 + c);
-		c++;
-	}
-	putchar('\n');
-	return (0);
-
+int c = 0;
+while (c < 10)
+{
+putchar(48 + c);
+c++;
+}
+putchar('\n');
+return (0);
 }
