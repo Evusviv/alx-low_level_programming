484d917881ab1a26ef5b08dc1010ac46ab60e039
Evusviv <evusviv@gmail.com>
2022-03-12 08:36:06 -0800

    5PrntNumbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - print all single numbers from 0-9,
+ * main - main block
+ * Description: print all single digits of numbers of base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-char x = '0';
-int x = 0;
-while ( x < '10');
-while ( x < 10 )
-{
-putchar(x);
-printf("%d", x);
-x++;
-}
-putchar('\n');
-printf("\n");
-return (0);
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+		return (0);
 }
