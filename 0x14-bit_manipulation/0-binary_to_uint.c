09aada865294cbc3d1b9d3030a1f97467f0a31e5
Evusviv <evusviv@gmail.com>
2022-04-29 09:12:12 +0100

    Convert binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -20,19 +19,4 @@ dec_val = 2 * dec_val + (b[i] - '0');
 }
 return (dec_val);
 }
-int main(void)
-{
-unsigned int n;
 
-n = binary_to_uint("1");
-printf("%u\n", n);
-n = binary_to_uint("101");
-printf("%u\n", n);
-n = binary_to_uint("1e01");
-printf("%u\n", n);
-n = binary_to_uint("1100010");
-printf("%u\n", n);
-n = binary_to_uint("0000000000000000000110010010");
-printf("%u\n", n);
-return (0);
-}
